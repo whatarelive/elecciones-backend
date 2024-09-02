6cd203600bc03736301a692cf32e2a38321b3345
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2024-09-02 05:05:06 +0200

    Configuracion del linter y creacion de Rutas de los Diputados

--- build/index.js
+++ build/index.js
@@ -2,12 +2,24 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = require("dotenv");
+const diputados_1 = require("./routers/diputados");
+// Creacion de la aplicacion de Express.
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
-const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 1234;
+// Configuariones de seguridad.
+app.disable('x-powered-by');
+// Middlewares
+app.use(express_1.default.json()); // Configuracion para convertir el body a JSON.
+app.use((0, cors_1.default)()); // Configuracion previa de Cors.
+// Configuracion de Routers.
+app.use('/api/diputado', diputados_1.diputadosRouter); // Responde a todas las rutas del recurso [diputados].
+// Configuracion del puerto de escucha.
+(0, dotenv_1.config)();
+const PORT = process.env.PORT ?? 4001;
+// Creacion del puerto de escucha de la aplicacion de Express.
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
-import express from "express";
-import cors from "cors";
-import { config } from "dotenv";
-import { deputyRouter } from "./routers/deputy";
+import express from 'express'
+import cors from 'cors'
+import { config } from 'dotenv'
+import { diputadosRouter } from './routers/diputados'
 
 // Creacion de la aplicacion de Express.
-const app = express();
+const app = express()
 
 // Configuariones de seguridad.
-app.disable("x-powered-by");
+app.disable('x-powered-by')
 
 // Middlewares
-app.use(express.json()); // Configuracion para convertir el body a JSON.
-app.use(cors()); // Configuracion previa de Cors.
+app.use(express.json()) // Configuracion para convertir el body a JSON.
+app.use(cors()) // Configuracion previa de Cors.
 
 // Configuracion de Routers.
-app.use("/api/diputado", deputyRouter); // Responde a todas las rutas del recurso [diputados].
+app.use('/api/diputado', diputadosRouter) // Responde a todas las rutas del recurso [diputados].
 
 // Configuracion del puerto de escucha.
-config();
+config()
+const PORT = process.env.PORT ?? 4001
 
 // Creacion del puerto de escucha de la aplicacion de Express.
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
-});
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
